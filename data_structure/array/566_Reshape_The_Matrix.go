ae63e9cd41a71b628b41335996ace37f4c963317
shengboyuan <15515272916@163.com>
2023-02-06 19:02:34 +0800

    11

---
+++ data_structure/array/566_Reshape_The_Matrix.go
@@ -0,0 +1,22 @@
+package array
+
+func MatrixReshape(mat [][]int, r int, c int) [][]int {
+	m, n := len(mat), len(mat[0])
+	if m*n != r*c {
+		return mat
+	}
+
+	res := make([][]int, 0)
+	for i, t := range mat {
+		for j, num := range t {
+			index := n*i + j + 1
+
+		}
+	}
+
+	return nil
+}
+
+func getIndex(num int, r int, c int) (int, int) {
+	return (num / r) - 1, num % c
+}
